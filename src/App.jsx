35e2ce49ea54478179b089436fd8c2098ba321c9
unknown <allesterabraham77@gmail.com>
2025-03-29 01:26:48 +0530

    workspace setup with TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div clssName="flex">
+      <h1 className="font-bold bg-green-300 text-blue-400">Hello Let's Begin</h1>
+    </div>
+  );
+}
+
+export default App;
